c9a6a696f44b91eeceb4811e9a7a5eb205001bc0
yalong-tse <yalong.tse@gmail.com>
2013-03-15 08:55:56 +0800

    init version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geodrilling::Application.config.secret_token = '00745e15614cd6b110e9db12c891d6b3ea83a156d92cb103e9403ec5b4cba4b9f412a8c1fa42b7b92024e1f7a7a2adbcc688f1930a82176d3b3f528bd1216635'
