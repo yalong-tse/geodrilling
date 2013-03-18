0b4198fd76f6bed5738c89e8f07d3e06e0989ba2
yalong-tse <yalong.tse@gmail.com>
2013-03-18 14:14:27 +0800

    20130318noon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
+#encoding: utf-8
 module ApplicationHelper
+  #进行 tab 切换的方法
   def setActiveTab(whichtag)
     code = "$().ready(function(){"
     code << "$('##{whichtag}').attr('class','active')"
     code << "})"
     javascript_tag(code);
+  end
+  #返回上一个页面
+  def link_back
+    link_to "返回",request.env["HTTP_REFERER"].blank?? "/":request.env["HTTP_REFERER"], :class=>"btn"
   end
 end
