8e67831b169698a304e4b8261dc69e4a4d87d487
yalong-tse <yalong.tse@gmail.com>
2013-04-17 21:41:26 +0800

    20130417

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -4,7 +4,7 @@ class ContractsController < ApplicationController
   # GET /contracts
   # GET /contracts.json
   def index
-    @contracts = Contract.all
+    @contracts = Contract.paginate(:page=>params[:page],:per_page=>5).all
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @contracts }
