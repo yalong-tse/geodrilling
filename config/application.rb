4c0f522625d403420277751a6ab5f400f861ba5c
niu_hr <nhr407@gmail.com>
2013-05-14 15:24:46 +0800

    add locale file examples

--- config/application.rb
+++ config/application.rb
@@ -30,9 +30,9 @@ module Geodrilling
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.default_locale = :zh_CN
+    
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
