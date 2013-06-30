a07a7f6283173518456282bc36e425be8032881b
yalong-tse <yalong.tse@gmail.com>
2013-06-30 23:54:40 +0800

    20130630night04

--- app/helpers/equipment_helper.rb
+++ app/helpers/equipment_helper.rb
@@ -1,8 +1,2 @@
 module EquipmentHelper
-  def make_datepicker(args)
-    code = "$(function (){"
-    code << "$('#{args}').datepicker({dateFormat:'yy-mm-dd'});"
-    code << "});"
-    javascript_tag(code);
-  end
 end
--- app/helpers/materials_helper.rb
+++ app/helpers/materials_helper.rb
@@ -1,8 +1,2 @@
 module MaterialsHelper
-  def make_datepicker(args)
-    code = "$(function (){"
-    code << "$('#{args}').datepicker({dateFormat:'yy-mm-dd'});"
-    code << "});"
-    javascript_tag(code);
-  end
 end
