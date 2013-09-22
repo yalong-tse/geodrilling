48c4086b56e873c8e281dfaa071996c9ad044063
yalong-tse <yalong.tse@gmail.com>
2013-09-22 21:55:15 +0800

    tourreports tables optimize

--- config/routes.rb
+++ config/routes.rb
@@ -74,6 +74,8 @@ Geodrilling::Application.routes.draw do
   post "archive/holearchive"
 
   get "tourreports/indexformodify"
+  get "tourreports/details"
+
   resources :tourreports
 
   get "holelist/index"
