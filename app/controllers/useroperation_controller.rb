2f8b3f5a9a44c2c28e6990b397ba1d7b992387d4
niu_hr <nhr407@gmail.com>
2013-10-17 01:06:50 +0800

    modify authorization

--- app/controllers/useroperation_controller.rb
+++ app/controllers/useroperation_controller.rb
@@ -1,5 +1,5 @@
 class UseroperationController < ApplicationController
-
+  load_and_authorize_resource
 
   def index
     @useroperations = Useroperation.all
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,7 +1,11 @@
+# encoding: utf-8
 class Ability
   include CanCan::Ability
 
   def initialize(user)
+    if user.account == 'admin'  # admin 用户不属于任何部门
+      can :manage, :all
+    end
     user_permission = []
     user.roles.each do |role|
       role.permissions.each do |permission|
